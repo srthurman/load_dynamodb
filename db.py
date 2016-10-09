822b935df24f71ab67b6b1523599ceb27573e39f
Sara Thurman <srthurman@gmail.com>
2016-10-09 19:28:10 -0400

    Improve db error handling

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import logging
 import boto3
 
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
@@ -8,38 +9,41 @@ def insert_data(table_name, data):
         for row in data:
             table.put_item(Item=row)
     except Exception as e:
-        print(e)
+        logging.exception('Error inserting data into the database')
 
 
 
-def create_table():
-    table = dynamodb.create_table(
-        TableName='Places',
-        KeySchema=[
-            {
-                'AttributeName': 'ID',
-                'KeyType': 'HASH'  #Partition key
-            },
-            {
-                'AttributeName': 'Place_Name',
-                'KeyType': 'RANGE'
+def create_table(table_name):
+    try:
+        table = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'ID',
+                    'KeyType': 'HASH'  #Partition key
+                },
+                {
+                    'AttributeName': 'Place_Name',
+                    'KeyType': 'RANGE'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'ID',
+                    'AttributeType': 'S'
+                },
+                {
+                    'AttributeName': 'Place_Name',
+                    'AttributeType': 'S'
+                }
+
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
             }
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'ID',
-                'AttributeType': 'S'
-            },
-            {
-                'AttributeName': 'Place_Name',
-                'AttributeType': 'S'
-            }
+        )
 
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 10,
-            'WriteCapacityUnits': 10
-        }
-    )
-
-    print("Table status:", table.table_status)
+        print('Table "{}" status:'.format(table_name), table.table_status)
+    except Exception as e:
+        logging.exception('Error creating table "{}"'.format(table_name))
